01415c56fd3ca858f2eabdcad10e83958793ec08
ashinwz <297787884@qq.com>
2023-07-17 22:43:25 +0800

    add tailwind header

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-red-700 text-2xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="mx-auto">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+    <p>
+        sdfsdf
+    </p>
+
+</template>
